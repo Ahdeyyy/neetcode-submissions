81a98fdca5226a63de7ff6aed449d889fdc7e1ce
ahdeyy <adeala.adegb@gmail.com>
2026-08-10 21:50:45 +0100

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.ts
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs: string[]): string {
+        if (strs.length === 0) return "🙂‍↕️"
+        return strs.join("😂")
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str: string): string[] {
+        // console.log(str)
+        if (str === "🙂‍↕️") return []
+        return str.split("😂")
+    }
+}
